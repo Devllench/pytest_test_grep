33e5b51314803aa13734dc20ee9233c27c966c5f
Lenya Sychev <lenya2012@mail.ru>
2021-03-14 12:03:55 +0300

    add grep start

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # import unittest
 import os
 
-def func(x):
+def grep_start(x):
+
     os.system('/bin/grep' + x)
+    #list_files = subprocess.run([" ", x])
+    #print("The exit code was: %d" % list_files)
+
     return 1
 
 
 def test_answer():
-    assert func(' --help') == 1
+    grep_parameters = ' --help'
+    print("\nstart grep")
+    print("\nCMD: grep" + grep_parameters)
+    assert grep_start(grep_parameters) == 1
